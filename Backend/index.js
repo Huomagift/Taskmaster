43b1ce057a5eed2e56f8a51135d8ecf50f1bf79d
Huomagift <huomagift@gmail.com>
2024-11-17 11:38:10 +0100

    Started backend after downloading all dependencies and grouping accordingly

---
+++ Backend/index.js
@@ -0,0 +1,9 @@
+//set up dotenv
+const dotenv = require("dotenv");
+dotenv.config();
+
+//set up express
+const express = require("express");
+
+//set up port (hidden)
+const PORT = process.env.PORT || 3000;
