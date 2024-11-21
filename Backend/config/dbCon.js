46caadb19e1f1cfe37b09a89064f81ff7b145ea8
Huomagift <huomagift@gmail.com>
2024-11-21 14:29:08 +0100

    connected to database

--- Backend/config/dbCon.js
+++ Backend/config/dbCon.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.DATABASE_URL);
+    try{
+        await mongoose.connect(process.env.DATABASE_URL);
+    } catch (error) {
+        console.log(error);  
+    } 
 }
+
+module.exports = connectDB;
+
--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,20 @@
 //set up dotenv
 const dotenv = require("dotenv");
 dotenv.config();
+const mongoose = require("mongoose");
 
 //set up express
 const express = require("express");
+app = express();
 
 //set up port (hidden)
 const PORT = process.env.PORT || 3000;
+
+const connectDB = require("./config/dbCon");
+connectDB();
+
+//connect to database
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+});
